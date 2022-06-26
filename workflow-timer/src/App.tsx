380dcb20f781cf6eb2469302e5e57f9b44a1cb8c
priscillasrg <priscilla.srg@gmail.com>
2022-06-26 18:26:29 -0300

    btn, form, list

--- workflow-timer/src/App.tsx
+++ workflow-timer/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Form from './components/Form/index';
+import List from './components/List';
 
 
 function App() {
   return (
     <div className="App">
-     Hello world!
+      <Form/>
+      <List/>
     </div>
   );
 }
